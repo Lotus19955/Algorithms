7eea537e7322eae4065fe12712eca8b32a588093
Lotus19955 <lotus19955@gmail.com>
2021-03-31 18:45:04 +0300

    Task1

---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shuffle;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "codeleet";
+            int[] indices = { 4, 5, 6, 7, 0 ,2, 1, 3, };
+
+            string x = ArrayShuffle.ShuffleString(word, indices);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson8/ShuffleString/Shuffle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle
+{
+    public class ArrayShuffle
+    {
+        /// <summary>
+        /// The method which swap letter in the word
+        /// </summary>
+        /// <param name="word">Word you want to shuffle</param>
+        /// <param name="indices">Array of integers</param>
+        public static string ShuffleString(string word, int[] indices)
+        {
+            char[] wordArray = new char[word.Length];
+
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                wordArray[indices[i]] = word[i];
+            }
+            return new string (wordArray);
+        }
+    }
+}
+
