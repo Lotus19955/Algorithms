0ea6c95461e7bf42049a441e69a54f4d4e69192c
Lotus19955 <lotus19955@gmail.com>
2021-04-01 18:33:36 +0300

    fix method IF

---
+++ Lesson8/Task2/Lesson8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shuffle;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Your Array must be the same Langth as integer Array, and Array indices should not be more than a long word 
+            string word = "codeleet";                           
+            int[] indices = { 4, 5, 6, 7, 0 ,2, 1, 3, };
+
+            string x = ArrayShuffle.ShuffleString(word, indices);
+            if (x != null)
+            {
+                Console.WriteLine(x);
+            }
+            else 
+            {
+                Console.WriteLine("Your array does not match the conditions");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson8/Task3/JewelsInStones/SearchJewels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelsInStones
+{
+    public class SearchJewels
+    {
+        public static void Searcher (string stones, string jewels)
+        {
+            int counter = 0;
+            foreach (char i in stones)
+            {
+                foreach (char j in jewels)
+                {
+                    if (j == i)
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
