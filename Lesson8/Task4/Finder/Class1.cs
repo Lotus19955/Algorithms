ac9e0cc3b19c104d937208ddf508325ed8748261
Lotus19955 <lotus19955@gmail.com>
2021-04-03 17:08:05 +0300

    New Task3

---
+++ Lesson8/Task4/Finder/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finder
+{
+    public class ArrayMethods
+    {
+        /// <summary>
+        /// Find vowels in word witout duplicates
+        /// </summary>
+        /// <param name="word">String variable</param>
+        /// <returns>Numbers of vowels in your word</returns>
+        public static int FindVowelsCount(string word)
+        {
+            char[] vowelsArray= {'e', 'y', 'u', 'i', 'o', 'a'};
+            int counter = 0;
+            foreach (char x in word)
+            {
+                foreach (char i in vowelsArray)
+                {
+                    if (x == i)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+        /// <summary>
+        /// Find vowels in word
+        /// </summary>
+        /// <param name="word">String variable</param>
+        /// <returns>Numbers of vowels in your word witout duplicates</returns>
+        public static int FindVowelsCountWithoutDuplicates(string word)
+        {
+            if (ValidateArray(word) == false)
+            {
+                return -1;
+            }
+
+            char[] vowelsArray = { 'e', 'y', 'u', 'i', 'o', 'a' };
+            char[] arrayWithoutDuplicate = new char[0];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j < vowelsArray.Length; j++)
+                {
+                    if (word[i] == vowelsArray[j])
+                    {
+                        Array.Resize(ref arrayWithoutDuplicate, arrayWithoutDuplicate.Length + 1);
+                        arrayWithoutDuplicate[0] = vowelsArray[j];
+                        arrayWithoutDuplicate[0]++;
+                    }
+                }
+            }
+
+            Array.Resize(ref arrayWithoutDuplicate, arrayWithoutDuplicate.Length - 1);
+            int counter = arrayWithoutDuplicate.Length;
+            return counter;
+        }
+        private static bool ValidateArray(string Word)
+        {
+            if (Word != null)
+            {
+                if (Word.Length > 0 )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Lesson8/Task4/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Finder;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "2aa3bbb1y2rr";
+            int value = Finder.ArrayMethods.FindVowelsCount(word);
+            int valueWithoutDuplicates = Finder.ArrayMethods.FindVowelsCountWithoutDuplicates(word);
+            Console.WriteLine($"Гласные с дубликатом: {value}");
+            Console.WriteLine($"Гласные с без дубликатов: {valueWithoutDuplicates}");
+            Console.ReadLine();
+        }
+    }
+}
