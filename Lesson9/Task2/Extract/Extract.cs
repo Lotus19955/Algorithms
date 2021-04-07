88a9c36180595ec9a8b9ee6332e607fb60d24b4b
Lotus19955 <lotus19955@gmail.com>
2021-04-07 18:36:57 +0300

    Add Task 2

---
+++ Lesson9/Task2/Extract/Extract.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLExtract
+{
+    public class Extract
+    {
+        /// <summary>
+        /// Finds host name in string
+        /// </summary>
+        /// <param name="site">Input string</param>
+        /// <returns>string</returns>
+        public static string ExtractsHostName (string site)
+        {
+            if (ValidateString(site) == false)
+            {
+                return null;
+            }
+
+            char[] chrSite = new char[0];
+            int counter = 0;
+
+            for (int i = 8; i < site.Length; i++)
+            {
+                Array.Resize(ref chrSite, chrSite.Length + 1);
+                chrSite[counter] = site[i];
+                counter++;
+            }
+            Array.Resize(ref chrSite, chrSite.Length - 1);
+            string result = new string(chrSite);
+            return result;
+        }
+
+        /// <summary>
+        /// Finds protocol in string
+        /// </summary>
+        /// <param name="site">Input string</param>
+        /// <returns>string</returns>
+        public static string ExtractsProtocol(string site)
+        {
+            if (ValidateString(site) == false)
+            {
+                return null;
+            }
+
+            char[] chrSite = new char[0];
+
+            for (int i = 0; i < site.Length; i++)
+            {
+                if (site[i] ==':')
+                {
+                    break;
+                }
+                Array.Resize(ref chrSite, chrSite.Length + 1);
+                chrSite[i] = site[i];
+            }
+            string result = new string(chrSite);
+            return result;
+        }
+
+        /// <summary>
+        /// Validates string
+        /// </summary>
+        /// <param name="word">Input string</param>
+        /// <returns>true or false</returns>
+        private static bool ValidateString(string word)
+        {
+            if (word != null)
+            {
+                if (word.Length > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Lesson9/Task2/Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URLExtract;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string site1 = "https://www.onliner.by/";
+            string site1HostName = URLExtract.Extract.ExtractsHostName(site1);
+            if (site1HostName != null)
+            {
+                Console.WriteLine(site1HostName);
+            }
+            else
+            {
+                Console.WriteLine("Your array does not match the conditions or empty");
+            }
+
+            string site2 = "https://mail.google.com/";
+            string site2HostName = URLExtract.Extract.ExtractsHostName(site2);
+            if (site2HostName != null)
+            {
+                Console.WriteLine(site2HostName);
+            }
+            else
+            {
+                Console.WriteLine("Your array does not match the conditions or empty");
+            }
+
+            string site3 = "https://www.onliner.by/";
+            string site3Protocol = URLExtract.Extract.ExtractsProtocol(site3);
+            if (site3Protocol != null)
+            {
+                Console.WriteLine(site3Protocol);
+            }
+            else
+            {
+                Console.WriteLine("Your array does not match the conditions or empty");
+            }
+            string site4 = "http://mail.google.com/";
+            string site4Protocol = URLExtract.Extract.ExtractsProtocol(site4);
+            if (site4Protocol != null)
+            {
+                Console.WriteLine(site4Protocol);
+            }
+            else
+            {
+                Console.WriteLine("Your array does not match the conditions or empty");
+            }
+            Console.ReadLine();
+        }
+    }
+}
